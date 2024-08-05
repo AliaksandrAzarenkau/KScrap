1d6a6604d263330ba1448eeecc8dbfeb14f90396
AliaksandrAzarenkau <alexaz1991@gmail.com>
2024-08-05 23:36:44 +0300

    Complete basic version 0.1

--- db_utils.py
+++ db_utils.py
@@ -1,2 +1,68 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db', check_same_thread=False)
+cursor = connection.cursor()
+
+
+def db_user_create():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    tg_id INTEGER,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    UNIQUE(id, tg_id)
+    )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+
+def db_notebooks_create():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS notebook (
+    id INTEGER PRIMARY KEY,
+    nb_url TEXT NOT NULL,
+    UNIQUE(id, nb_url)
+    )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+
 def user_register(user_data: dict) -> None:
-    pass
+    cursor.execute('SELECT tg_id FROM users WHERE tg_id = ?', (user_data['user_id'],))
+    user = cursor.fetchone()
+
+    if not user:
+        cursor.execute(
+            'INSERT OR IGNORE INTO Users (tg_id, first_name, last_name) VALUES (?, ?, ?)',
+            (
+                user_data['user_id'],
+                user_data['user_first_name'],
+                user_data['user_last_name']))
+
+    connection.commit()
+
+
+def nb_items_register(items: list) -> list:
+    current_list = []
+    new_items_list = []
+
+    cursor.execute('SELECT * FROM notebook', )
+    item_list = cursor.fetchall()
+
+    for _ in item_list:
+        current_list.append(_[1])
+
+    for item in items:
+        if item not in current_list:
+            cursor.execute('INSERT OR IGNORE INTO notebook (nb_url) VALUES (?)',
+                           (item,))
+            new_items_list.append(item)
+
+    connection.commit()
+
+    return new_items_list
--- tbot.py
+++ tbot.py
@@ -1,17 +1,20 @@
-import telebot
+import asyncio
 
+from telebot.async_telebot import AsyncTeleBot
+
+from scraper import scrape_all, create_nb_item_links
 from db_utils import user_register
 from cfg import TG_TOKEN
+from scraper_cfg import NB_URL
 
 API_TOKEN = TG_TOKEN
 
-bot = telebot.TeleBot(API_TOKEN)
+bot = AsyncTeleBot(TG_TOKEN)
 
 
-# Handle '/start' and '/help'
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, 'хуярт')
+async def send_welcome(message):
+    await bot.reply_to(message, 'хуярт')
     user_info = {
         'user_id': message.from_user.id,
         'user_first_name': message.from_user.first_name,
@@ -19,4 +22,26 @@ def send_welcome(message):
     user_register(user_info)
 
 
-bot.infinity_polling()
+async def bot_send_items_to_me(item: str):
+    await bot.send_message(770325923, item)
+
+
+async def scraping_task():
+    resp = await scrape_all(NB_URL)
+    to_send = await create_nb_item_links(resp)
+
+    for _ in to_send:
+        await bot_send_items_to_me(_)
+
+
+async def periodic():
+    while True:
+        await scraping_task()
+        await asyncio.sleep(60)
+
+
+async def main():
+    await asyncio.gather(bot.infinity_polling(), periodic())
+
+if __name__ == '__main__':
+    asyncio.run(main())
