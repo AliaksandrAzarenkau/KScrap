e7db0aaf4e821b88ca7469f8384514ce35cc606d
AliaksandrAzarenkau <alexaz1991@gmail.com>
2024-11-02 17:32:02 +0300

    Refactoring bot. Add comments.

--- scraper.py
+++ scraper.py
@@ -1,13 +1,18 @@
 import requests
+import urllib.parse
+from langdetect import detect
 
-from db_utils import my_nb_items_register, horse_tv_items_register, coffee_items_register
-
-nb_item_links = []
-horse_tv_item_links = []
-coffee_item_links = []
+from scraper_cfg import SEARCH_URL, SEARCH_URL_POSTFIX, REGION_DICT
+from db_utils import subscription_register, item_register, send_subscriptions_handler, items_checker, get_subscriptions, delete_subscription
 
 
-async def scrape_all(url):
+def scrape_all(url):
+    """
+    Get response from given url and return it as a JSON object.
+
+    :param url: target url
+    :return: JSON object
+    """
     r = requests.get(url)
 
     try:
@@ -18,41 +23,172 @@ async def scrape_all(url):
         return []
 
 
-async def create_my_nb_item_links(response) -> list:
-    try:
-        for ads in response['ads']:
-            nb_item_links.append(ads['ad_link'])
+def url_builder(region: int, to_search: str) -> str:
+    """
+    Build url based on region code and keywords to search.
 
-        new_items = my_nb_items_register(nb_item_links)
+    :param region: region code
+    :param to_search: keywords to search
+    :return: url string
+    """
+    if region == 0:
+        url = SEARCH_URL + f'query={to_search}' + SEARCH_URL_POSTFIX
+    else:
+        url = SEARCH_URL + f'query={to_search}&rgn={region}' + SEARCH_URL_POSTFIX
+
+    return url
+
+
+def encode_to_url(search_request: str) -> str:
+    """
+    Encode lang to URL lang.
+
+    :param search_request: keywords to search
+    :return: encoded string
+    """
+    encoded_string = urllib.parse.quote(search_request)
+    return encoded_string
+
+
+def language_detection(text: str) -> str:
+    """
+    Detect language of text. If isn't English, decode it to URL lang.
+
+    :param text: text to detect
+    :return: result text(decoded or as is)
+    """
+    detected_language = detect(text)
+
+    if detected_language != 'en':
+        result = encode_to_url(text)
+        return result
+
+    return text
+
+
+async def subscription_handler(request_text: str, telegram_id: int) -> str:
+    """
+    Handle subscription requests. Check subscription register. Handle subscription exceptions.
+
+    :param request_text:
+    :param telegram_id:
+    :return: string with handling tesult
+    """
+    region = 0
+    if request_text.count(',') != 1:
+        return 'Неправильный формат ввода'
+
+    subscription_request = request_text.split(', ')
+
+    for k, v in REGION_DICT.items():
+        if subscription_request[0].lower() == k:
+            region = v
+
+    encoded_keyword = subscription_request[1].lower()
+    to_search = language_detection(encoded_keyword)
+
+    url = url_builder(region, to_search)
+
+    scraped_data = scrape_all(url)
+
+    # Обработка исключений поиска и проверка наличия подписки
+    try:
+        # ads = scraped_data['ads']
+        check_subscription = subscription_register(region, encoded_keyword, to_search, telegram_id)
+        if check_subscription == 'Такая подписка уже активна':
+            return check_subscription
+        result = 'Рассылка активирована'
+        return result
+    except KeyError as e:
+        print(f'KeyError: {e}')
+        result = 'Поиск не дал результата. Уточни параметры или проверь по сайту.'
+        return result
+
+
+def to_send_preparation():
+    """
+    Prepare items data for sending to Telegram.
+
+    :return: generator object with items data for each user
+    """
+    subscriptions = send_subscriptions_handler()
+    for k, v in subscriptions.items():
+        sent_items_link_list = items_checker(k)
+        for _ in v:
+            url = url_builder(_[2], _[3])
+            item_url_list = link_extractor(url)
+
+            for link in item_url_list:
+                if link not in sent_items_link_list:
+                    item_register(tg_id=k, url=link)
+
+                    yield [k, link]
+
+
+def link_extractor(url: str) -> list:
+    """
+    Parsing item urls from API response for given url.
+    :param url: target url
+
+    :return: list of item urls
+    """
+    item_list = []
+    response = scrape_all(url)
+    try:
+        ads = response['ads']
+        for ad in ads:
+            link = ad['ad_link']
+            item_list.append(link)
+
+        return item_list
 
     except Exception as e:
-        print(e)
-        new_items = []
-
-    return new_items
+        print('link_extractor: ', e)
 
 
-async def create_horse_tv_item_links(response) -> list:
+def subscriptions_list_handler(telegram_id: int) -> str | None:
+    """
+    Get list of subscriptions for given telegram id.
+    :param telegram_id:
+
+    :return: None or list of subscriptions
+    """
+    count = 1
+    current_subscriptions_list = []
+    current_subscriptions = get_subscriptions(telegram_id)
+
+    if current_subscriptions:
+        for _ in current_subscriptions:
+            for k, v in REGION_DICT.items():
+                if _[2] == v:
+                    sub_item = [k, _[3]]
+                    current_subscriptions_list.append(sub_item)
+        result = ''
+        for subscription in current_subscriptions_list:
+            result += str(count) + ') ' + subscription[0] + ', ' + subscription[1] + '\n'
+            count += 1
+        return result
+
+    return None
+
+
+def subscription_del(telegram_id: int, subscription_number: int) -> str:
+    """
+    Delete given subscription.
+
+    :param telegram_id:
+    :param subscription_number: number of the subscription chosen by user
+    :return: string with result of deleting subscription
+    """
+    current_subscriptions = get_subscriptions(telegram_id)
     try:
-        for ads in response['ads']:
-            horse_tv_item_links.append(ads['ad_link'])
+        to_del = current_subscriptions[subscription_number - 1]
+    except IndexError:
+        return 'Неверный формат ввода.\nЗапусти команду заново.'
 
-        new_items = horse_tv_items_register(horse_tv_item_links)
-    except Exception as e:
-        print(e)
-        new_items = []
+    result = delete_subscription(to_del[0])
 
-    return new_items
+    if result:
+        return 'Что-то пошло не так'
 
-
-async def create_coffee_item_links(response) -> list:
-    try:
-        for ads in response['ads']:
-            coffee_item_links.append(ads['ad_link'])
-
-        new_items = coffee_items_register(coffee_item_links)
-    except Exception as e:
-        print(e)
-        new_items = []
-
-    return new_items
+    return 'Удалена'
--- tbot.py
+++ tbot.py
@@ -1,64 +1,131 @@
 import asyncio
 
-from telebot.async_telebot import AsyncTeleBot
+from telebot.async_telebot import AsyncTeleBot, asyncio_filters, types
+from telebot.asyncio_storage import StateMemoryStorage
+from telebot.states import State, StatesGroup
+from telebot.states.asyncio.context import StateContext
+from telebot.types import ReplyParameters
 
-from scraper import scrape_all, create_my_nb_item_links, create_horse_tv_item_links, create_coffee_item_links
-from db_utils import user_register, db_user_create, db_my_notebooks_create, db_horse_tv_create, db_my_coffee_create
 from cfg import TG_TOKEN
-from scraper_cfg import MY_NB_URL, HORSE_TV_URL, NIVONA_COFFEE_URL, MELITTA_COFFEE_URL
+from db_utils import db_user_create, db_subscription_create, db_items_create, user_register
+from scraper import subscription_handler, to_send_preparation, subscriptions_list_handler, subscription_del
 
 API_TOKEN = TG_TOKEN
 
-bot = AsyncTeleBot(TG_TOKEN)
+state_storage = StateMemoryStorage()
+bot = AsyncTeleBot(TG_TOKEN, state_storage=state_storage)
+
+
+class MyStates(StatesGroup):
+    choice = State()
+    end = State()
 
 
 @bot.message_handler(commands=['start'])
 async def send_welcome(message):
-    await bot.reply_to(message, 'хуярт')
-    user_info = {
-        'user_id': message.from_user.id,
-        'user_first_name': message.from_user.first_name,
-        'user_last_name': message.from_user.last_name}
+    """
+    Start and register user(or ignore)
+    """
+    await bot.reply_to(message,
+                       '*Формат запроса поиска: "город, ключевое слово(фраза)".*\nОбязательно через одну '
+                       'запятую после города\n*пример:* ***брест, гладильная доска*** или ***беларусь, '
+                       'блоки газосиликатные*** (на регистр пофиг)\n \n Доступные регионы: Областные центры, Беларусь\n'
+                       '\nДоступные команды: /start, /list, /del\n'
+                       'start - запуск, он же help\n list - список активных подписок \n del - удаление подписки',
+                       parse_mode='Markdown')
+    user_info = message.from_user.id
     user_register(user_info)
 
 
-async def bot_send_items_to_me(item: str):
-    await bot.send_message(770325923, item)
+@bot.message_handler(commands=['list'])
+async def get_subscription_list(message):
+    """
+    Add subscription if not exist
+    """
+    user_info = message.from_user.id
+    current_subscriptions = subscriptions_list_handler(user_info)
+
+    if current_subscriptions:
+        await bot.reply_to(message, current_subscriptions)
+
+    else:
+        await bot.reply_to(message, 'Нет активных подписок')
 
 
-async def bot_send_items_to_horse(item: str):
-    await bot.send_message(772495588, item)
+@bot.message_handler(commands=['del'])
+async def delete_subscription_handler(message: types.Message, state: StateContext):
+    """
+    Delete subscription(start point of dialog chain
+    """
+    await state.set(MyStates.choice)
+
+    user_info = message.from_user.id
+    current_subscriptions = subscriptions_list_handler(user_info)
+
+    if current_subscriptions:
+        text = '\n Отправь номер подписки чтобы удалить (тупо цыфра)'
+        await bot.send_message(
+            user_info,
+            current_subscriptions + text,
+            reply_parameters=ReplyParameters(message_id=message.message_id))
+    else:
+        await bot.reply_to(message, 'Нет активных подписок')
 
 
-def on_startup():
+@bot.message_handler(state=MyStates.choice)
+async def delete_subscription(message: types.Message, state: StateContext):
+    """
+    Result of deleting subscription
+    """
+    text = message.text
+    result = ''
+
+    try:
+        choice = int(text)
+        result = subscription_del(message.from_user.id, choice)
+    except ValueError as e:
+        print('Delete error: ', e)
+
+    await bot.send_message(
+        message.from_user.id,
+        result,
+        reply_parameters=ReplyParameters(message_id=message.message_id))
+    await state.delete()
+
+
+@bot.message_handler(func=lambda message: True)
+async def add_subscription(message):
+    """
+    add subscription
+    """
+    user_info = message.from_user.id
+    message_text = message.text
+
+    result = await subscription_handler(message_text, user_info)
+    await bot.reply_to(message, result)
+
+
+@bot.message_handler(func=lambda message: True)
+async def bot_send_items(message: str, user_info: int):
+    """
+    Send item to user
+    """
+    await bot.send_message(text=message, chat_id=user_info)
+
+
+def on_startup(): # Create tables on startup if not exists
     db_user_create()
-    db_my_notebooks_create()
-    db_my_coffee_create()
-    db_horse_tv_create()
+    db_subscription_create()
+    db_items_create()
 
 
 async def scraping_task():
-    resp = await scrape_all(MY_NB_URL)
-    to_send_for_me = await create_my_nb_item_links(resp)
-
-    resp = await scrape_all(HORSE_TV_URL)
-    to_send_for_horse = await create_horse_tv_item_links(resp)
-
-    resp = await scrape_all(NIVONA_COFFEE_URL)
-    to_send_about_nivona = await create_coffee_item_links(resp)
-    resp = await scrape_all(MELITTA_COFFEE_URL)
-    to_send_about_melitta = await create_coffee_item_links(resp)
-
-    for _ in to_send_for_me:
-        await bot_send_items_to_me(_)
-
-    for _ in to_send_about_nivona:
-        await bot_send_items_to_me(_)
-    for _ in to_send_about_melitta:
-        await bot_send_items_to_me(_)
-
-    for _ in to_send_for_horse:
-        await bot_send_items_to_horse(_)
+    """
+    Tasks for scraping data
+    """
+    ads = to_send_preparation()
+    for ad in ads:
+        await bot_send_items(user_info=ad[0], message=ad[1])
 
 
 async def periodic():
@@ -68,6 +135,11 @@ async def periodic():
 
 
 async def main():
+    bot.add_custom_filter(asyncio_filters.StateFilter(bot))  # Need to States
+
+    from telebot.states.asyncio.middleware import StateMiddleware  # Need to States
+    bot.setup_middleware(StateMiddleware(bot))
+
     await asyncio.gather(bot.infinity_polling(), periodic())
 
 
